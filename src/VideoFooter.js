3caad77364210731f0e4aeef6a1941a6957ccf00
Suraj Singh <50519406+surajsly@users.noreply.github.com>
2020-09-18 11:11:07 +0530

    react-ticker

--- src/VideoFooter.js
+++ src/VideoFooter.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./VideoFooter.css";
+import MusicNoteIcon from "@material-ui/icons/MusicNote";
 
 const VideoFooter = () => {
   return (
@@ -7,6 +8,9 @@ const VideoFooter = () => {
       <div className="videoFooter__text">
         <h3>@surajsly</h3>
         <p>this is some text</p>
+        <div classname="VideoFooter__ticker">
+          <MusicNoteIcon className="videoFooter__icon" />
+        </div>
       </div>
     </div>
   );
